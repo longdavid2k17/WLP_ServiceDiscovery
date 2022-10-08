f141e73a2a63f7adb08d164529ebba9d9db462b9
Dawid Kańtoch <kantochdawid98@gmail.com>
2022-10-08 23:53:46 +0200

    Cleaning sub pom.xml file, providing unit tests for NetworkConfigurationService class

---
+++ WLP_ServiceDiscovery_Core/src/test/pl/com/kantoch/WLP_ServiceDiscovery/service/NetworkConfigurationServiceTest.java
@@ -0,0 +1,32 @@
+package pl.com.kantoch.WLP_ServiceDiscovery.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.net.UnknownHostException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NetworkConfigurationServiceTest {
+    //private NetworkConfigurationService networkConfigurationService = Mockito.mock(NetworkConfigurationService.class);
+    @Autowired
+    private NetworkConfigurationService networkConfigurationService;
+
+    @Test
+    public void shouldReturnHostAddress() throws UnknownHostException {
+        String hostAddress = networkConfigurationService.getHostAddress();
+        assertNotNull(hostAddress);
+    }
+
+    @Test
+    public void shouldReturnHostName() throws UnknownHostException {
+        String hostName = networkConfigurationService.getHostName();
+        assertNotNull(hostName);
+    }
+}
